88cbd0d72dfe19e3a07b1c1be6df268a7b11cb4a
DarwinKopimi <nickablair@gmail.com>
2017-08-12 00:45:38 -0400

    updated simple calculator

--- pyCalculator/simplPyCalculator.py
+++ pyCalculator/simplPyCalculator.py
@@ -1,3 +1,8 @@
 print("Do some simple basic math as plus,subtract,multiple and divide")
-raw_input()
+print("Enter First Variable")
+user_input_one=raw_input()
+print("Operation to perform")
+operation=raw_input()
+print("Enter Second Variable")
+user_input_two=raw_input()
 #Actually do some operations with this class
